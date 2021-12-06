1ff88621bd331a2d1bc3b54d4a9bdf74d2af7521
hussainiftikhar5242 <95127768+hussainiftikhar5242@users.noreply.github.com>
2021-12-06 23:24:31 +0500

    I have done this work in which I take 2 number from user and then find the multiple of these 2 numbers.

---
+++ task23.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int num1,num2;
+	cout<<"enter 1st number";
+	cin>> num1;
+	cout<<"enter 2nd number";
+	cin>> num2;
+	cout<<"the multiple of 1st number is :"<<num1<<" "<<num1+num1<<" "<<num1+num1+num1<<num1+num1+num1+num1<<num1+num1+num1+num1+num1<<endl;
+	cout<<"the multiple of 2nd number is :"	<<num2<<num2+num2<<num2+num2+num2<<num2+num2+num2+num2<<num2+num2+num2+num2+num2<<endl;
+}
