f5b016db52b8819f4f9c10213da3381b89b85579
hussainiftikhar5242 <95127768+hussainiftikhar5242@users.noreply.github.com>
2021-12-06 23:20:14 +0500

    I have done this work in which I take charge and time values  form user and then find the current.

---
+++ task21.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	float charge,time;
+	cout<<"enter charge in coulum:";
+	cin>> charge;
+	cout<< "enter time in second:";
+	cin>> time;
+	float current==charge/time;
+	cout<<"current is"<< current;
+	
+
+	
+	
+}
